9643f160485831fc544d99773076a58c15548815
João Henrique <joaoh9.costa@gmail.com>
2021-02-27 17:54:08 -0300

    Refatorando o Teste de Unidade

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,38 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-
 from lists.models import Item, List
-from lists.views import home_page
-
-
-class HomePageTest(TestCase):
-
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
-
-
-class NewListTest(TestCase):
-
-    def test_can_save_a_POST_request(self):
-        self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        response = self.client.post(
-            '/lists/new', data={'item_text': 'A new list item'})
-        new_list = List.objects.first()
-        self.assertRedirects(response, '/lists/' + str(new_list.id) + '/')
 
 
 class ListAndItemModelsTest(TestCase):
