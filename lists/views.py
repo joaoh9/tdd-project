73565d97d31830a47d2f737f02ac1e28262ebf14
João Henrique <joaoh9.costa@gmail.com>
2021-02-27 15:50:13 -0300

    Processando Requisição POST

--- lists/views.py
+++ lists/views.py
@@ -2,8 +2,10 @@
 from __future__ import unicode_literals
 
 
-# Create your views here.
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
+    if request.method == 'POST':
+        return HttpResponse(request.POST['item_text'])
     return render(request, 'home.html')
