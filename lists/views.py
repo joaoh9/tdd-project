05a0d4102c48a0f134c7d272d2dbf26dc318690f
João Henrique <joaoh9.costa@gmail.com>
2021-02-27 14:56:21 -0300

    Escrevendo código da aplicação

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+
+# Create your views here.
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home_page():
+    pass
